f608fbded43bfd8b640dfebe1e8e73d6ce3a4d0c
Arpit4477 <132847627+Arpit4477@users.noreply.github.com>
2024-10-12 03:33:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -263,12 +263,12 @@ app.get('/api/dustbin-status', ensureAuthenticated, async (req, res) => {
         const statusPromises = dustbins.map(async dustbin => {
             const latestSensorData = await SensorData.findOne({ deviceID: dustbin.deviceId }).sort({ createdAt: -1 }).exec();
             if (latestSensorData) {
-                const sensors = [latestSensorData.sensor1, latestSensorData.sensor2];
+                const sensors = [latestSensorData.s1, latestSensorData.s2];
                 const maxSensorValue = Math.max(...sensors);
                 const fillLevel = maxSensorValue > 20 ? '100%' : maxSensorValue > 15 ? '75%' : maxSensorValue > 10 ? '50%' : '25%';
-                return { ...dustbin.toObject(), fillLevel, sensor1: latestSensorData.sensor1, sensor2: latestSensorData.sensor2, battery: latestSensorData.battery, voltage: latestSensorData.voltage };
+                return { ...dustbin.toObject(), fillLevel, s1: latestSensorData.s1, s2: latestSensorData.s2, b: latestSensorData.b, v: latestSensorData.v };
             } else {
-                return { ...dustbin.toObject(), fillLevel: '25%', sensor1: 0, sensor2: 0, battery: 0, voltage: 0 };
+                return { ...dustbin.toObject(), fillLevel: '25%', s1: 0, s2: 0, b: 0, v: 0 };
             }
         });
         const statuses = await Promise.all(statusPromises);
