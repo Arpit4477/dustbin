e36cee60ae32f7acc72446b08032b3b28baed816
Arpit4477 <132847627+Arpit4477@users.noreply.github.com>
2024-10-12 03:27:36 +0530

    Update SensorData.js

--- models/SensorData.js
+++ models/SensorData.js
@@ -2,14 +2,14 @@
 const mongoose = require('mongoose');
 
 const SensorDataSchema = new mongoose.Schema({
-    deviceID: {
+    ID: {
         type: String,
         required: true
     },
-    sensor1: Number,
-    sensor2: Number,
-    battery: Number,
-    voltage: Number,
+    s1: Number,
+    s2: Number,
+    b: Number,
+    v: Number,
     createdAt: {
         type: Date,
         default: Date.now
