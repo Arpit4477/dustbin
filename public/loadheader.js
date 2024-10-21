f92c505c5c67f40f3a941e6fe0520d9d0bfae7bb
Arpit4477 <132847627+Arpit4477@users.noreply.github.com>
2024-10-21 12:13:33 +0530

    Update loadheader.js

--- public/loadheader.js
+++ public/loadheader.js
@@ -1,7 +1,7 @@
 // loadHeader.js
 
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('header.ejs')
+    fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
